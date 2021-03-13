9be9971d7b8743833aaf8469e0e4d3b3ad9f6203
chenyifan.007 <chenyifan.007@bytedance.com>
2021-03-14 02:51:58 +0800

    20210313

--- Easy_7_Reverse/src/Solution.java
+++ Easy_7_Reverse/src/Solution.java
@@ -15,10 +15,10 @@ class Solution {
         while(x != 0){
             int pop = x%10;
             x = x/10;
-            if(res/10>Integer.MAX_VALUE/10 || (res/10 == Integer.MAX_VALUE/10 && pop >7)){
+            if(res>Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10 && pop >7)){
                 return 0;
             }
-            if(res/10<Integer.MIN_VALUE/10 || (res/10 == Integer.MIN_VALUE/10 && pop <-8)){
+            if(res<Integer.MIN_VALUE/10 || (res == Integer.MIN_VALUE/10 && pop <-8)){
                 return 0;
             }
             res = 10*res + pop;
